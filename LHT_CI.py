15a949699a783be97f6f8f5eb239f16fc3f44072
Carlos Moisés Hernández Suárez <62440295+car-git@users.noreply.github.com>
2023-04-04 11:22:26 -0600

    Add files via upload

--- LHT_CI.py
+++ LHT_CI.py
@@ -5,101 +5,96 @@ Created on Sun Mar 26 15:26:51 2023
 
 @author: carlosmh1
 """
+
 import pandas as pd
 import numpy as np
 import os
 from scipy.optimize import fsolve
 from scipy.stats import norm
 
-alpha = 0.05
-fnam = "test_data.csv"
-dir_path = "/Users/carlosmh1/Library/CloudStorage/Dropbox/Current_Research/Estimation of LHT/"
+# FUNCTIONS DECLARATION
 
-
-Z = norm.ppf(1-alpha/2)
-# print(Z)
+def clear_screen():
+    if os.name == 'nt':  # For Windows
+        os.system('cls')
+    else:  # For macOS and Linux
+        os.system('clear')
 
 def fun(x, t, g, c):
     z = np.exp(-x*t)
     return np.dot(z, g) - c
 
+# MODIFY PARAMETERS AS NEEDED
+alpha = 0.05
+fnam = "test_data.csv"
 
-# create an empty pandas dataframe with column names
-data = pd.DataFrame(columns=['t', 'n', 'h'])
+clear_screen()
+Z = norm.ppf(1-alpha/2)
 
-t = pd.Series(dtype=int)
-n = pd.Series(dtype=int)
-h = pd.Series(dtype=int)
-g = pd.Series(dtype='float64')
-f = pd.Series(dtype='float64')
 
-file = os.path.join(dir_path, fnam)
 
-df = pd.read_csv(fnam, header=0,skip_blank_lines=True, usecols=[0, 1, 2])
+# IMPORT DATA
+df = pd.read_csv(fnam, header=0, skip_blank_lines=True, usecols=[0, 1, 2])
 df.dropna(inplace=True)
-
-data['t'] = df.iloc[:, 0]
-data['n'] = df.iloc[:, 1]
-data['h'] = df.iloc[:, 2]
-# print(data)
 
 t = np.array(df['t'])
 n = np.array(df['n'])
 h = np.array(df['h'])
 
+# CALCULATE VALUES
 N = n[0]
 K = np.sum(h)
 g = h/K
 f = (n[:-1] - n[1:])/N
 f = np.append(f, 0)
 
-data['g'] = g
-data['f'] = f
-# print(data)
+# CREATE A NEW DATAFRAME
+data = pd.DataFrame({"t": t, "n": n, "h": h, "g": g, "f": f})
 
-name_parts = fnam.split('.')
-fnam2 = name_parts[0]+'_added.csv'
+# SAVE THE NEW DATAFRAME
+fnam2 = fnam.split('.')[0] + '_added.csv'
 data.to_csv(fnam2, index=False)
-print("")
-print("")
-print("")
-print('Initial number of individuals N : ', N)
-print('Offspring size K : ', K)
-# R0
-R0 = K/N
-print('R0 : ',R0)
 
-# Longevity 
+# CALCULATE R0
+R0 = K / N
+
+# CALCULATE LONGEVITY
 EL = np.dot(t, f)
-EL2 = np.dot(t*t, f)
-VL = EL2-EL*EL
-CI_L = np.array([EL , VL, EL-Z*np.sqrt(VL/N),EL+Z*np.sqrt(VL/N)])
-print('Longevity : ',CI_L)
+EL2 = np.dot(t * t, f)
+VL = EL2 - EL * EL
+CI_L = np.array([EL, VL, EL - Z * np.sqrt(VL / N), EL + Z * np.sqrt(VL / N)])
 
-# Generation time
+# CALCULATE GENERATION TIME
 ET = np.dot(t, g)
-ET2 = np.dot(t*t, g)
-VT = ET2-ET*ET
+ET2 = np.dot(t * t, g)
+VT = ET2 - ET * ET
+CI_mu = np.array([ET, VT, ET - Z * np.sqrt(VT / K), ET + Z * np.sqrt(VT / K)])
 
-CI_mu = np.array([ET , VT, ET-Z*np.sqrt(VT/K),ET+Z*np.sqrt(VT/K)])
-print('Generation time : ',CI_mu)
+# CALCULATE POPULATION GROWTH RATE, r
+x_initial_guess = 0.05
+r_sol = fsolve(fun, x_initial_guess, args=(t, g, 1 / R0), xtol=1e-6)
 
-# Population growth rate, r
-
-x_initial_guess = 0.05  # initial guess for x
-r_sol = fsolve(fun, x_initial_guess, args=(t, g, 1/R0),xtol = 1e-6)
-
-mu = 1/R0
-z = np.exp(-(2*r_sol)*t)
-s2 = np.dot(z, g) - mu**2
+mu = 1 / R0
+z = np.exp(-(2 * r_sol) * t)
+s2 = np.dot(z, g) - mu ** 2
 s = np.sqrt(s2)
 
-r_L_sol = fsolve(fun, r_sol, args=(t, g, mu+Z*s/np.sqrt(K)),xtol = 1e-6)
-r_U_sol = fsolve(fun, r_sol, args=(t, g, mu-Z*s/np.sqrt(K)),xtol = 1e-6)
+r_L_sol = fsolve(fun, r_sol, args=(t, g, mu + Z * s / np.sqrt(K)), xtol=1e-6)
+r_U_sol = fsolve(fun, r_sol, args=(t, g, mu - Z * s / np.sqrt(K)), xtol=1e-6)
 
-# Growth rates
+# GROWTH RATES
 CI_r = [r_sol[0], r_L_sol[0], r_U_sol[0]]
-print('r : ',CI_r)
 CI_lam = [np.exp(i) for i in CI_r]
-print('lambda: ',CI_lam)
-print('New data saved to: ',fnam2)
+
+# DISPLAY RESULTS
+
+
+print('Initial number of individuals N : ', N)
+print('Offspring size K : ', K)
+print('R0 : ', R0)
+print('Longevity : ', CI_L)
+print('Generation time : ', CI_mu)
+print('r : ', CI_r)
+print('lambda: ', CI_lam)
+print('New data saved to: ', fnam2)
+
--- LHT_CI.r
+++ LHT_CI.r
@@ -1,23 +1,31 @@
 #!/usr/bin/env Rscript
 
+# Load libraries
 library(readr)
 library(dplyr)
 library(tidyr)
 library(purrr)
 library(stats)
 
-# Modify this as needed :---------------
-alpha <- 0.05 # Change parameter as needed
-fnam <- "test_data.csv" # Change file name
-file <- fnam
-# Change directory path here, live commented if file is in current directory:
-dir_path <- getwd()   # use this if data is in current directory
-#file <- file.path(dir_path, fnam)  # ....or change directory here
+# MODIFY PARAMETERS AS NEEDED
+alpha <- 0.05
+fnam <- "test_data.csv"
+dir_path <- getwd()
 
-# Begin calculations :---------------
 
-Z <- qnorm(1-alpha/2)
-cat(Z, "\n")
+#FUNCTION DECLARATIONS 
+clear_screen <- function() {
+  os <- Sys.info()["sysname"]
+  
+  if (os == "Windows") {
+    system("cls")
+  } else if (os == "Linux" || os == "Darwin") {  # Darwin for macOS
+    cat("\014")
+  } else {
+    cat("Unsupported OS. Cannot clear screen.\n")
+  }
+}
+
 
 fun <- function(x, t, g, c) {
   th <- exp(-x*t)
@@ -25,44 +33,42 @@ fun <- function(x, t, g, c) {
 }
 
 
+# BEGIN CALCULATIONS
+Z <- qnorm(1-alpha/2)
 
-
-df <- read_csv(file, col_types = cols())
-
+# Read data
+df <- read_csv(fnam, col_types = cols())
 t <- df$t
 n <- df$n
 h <- df$h
 
+# Calculate variables
 N <- n[1]
 K <- sum(h)
 g <- h/K
-
 f <- c(n[-length(n)] - n[-1], 0)/N
 
-my_data <- data.frame(t,n,h,g,f)
+# Create new data frame
+my_data <- data.frame(t, n, h, g, f)
 fnam2 <- paste0(gsub(".csv", "", fnam), "_added.csv")
 write.csv(my_data, file = fnam2, row.names = FALSE)
 
-
-# R0
+# Calculate R0, Longevity, Generation time, and Population growth rate
 R0 <- K/N
 cat("R0 :", R0, "\n")
 
-# Longevity 
 EL <- sum(t*f)
 EL2 <- sum(t^2*f)
 VL <- EL2 - EL^2
 CI_L <- c(EL , VL, EL-Z*sqrt(VL/N), EL+Z*sqrt(VL/N))
 cat("Longevity :", CI_L, "\n")
 
-# Generation time
 ET <- sum(t*g)
 ET2 <- sum(t^2*g)
 VT <- ET2 - ET^2
 CI_mu <- c(ET, VT, ET-Z*sqrt(VT/K), ET+Z*sqrt(VT/K))
 cat("Generation time :", CI_mu, "\n")
 
-# Population growth rate, r
 x_initial_guess <- 0.05  # initial guess for x
 r_sol <- uniroot(function(x) fun(x, t, g, 1/R0), interval = c(0, 1), tol = 1e-6)$root
 
@@ -74,24 +80,29 @@ s <- sqrt(s2)
 r_L_sol <- uniroot(function(x) fun(x, t, g, mu+Z*s/sqrt(K)), interval = c(0, 1), tol = 1e-6)$root
 r_U_sol <- uniroot(function(x) fun(x, t, g, mu-Z*s/sqrt(K)), interval = c(0, 1), tol = 1e-6)$root
 
-CI_r<- c(r_sol, r_L_sol, r_U_sol)
+CI_r <- c(r_sol, r_L_sol, r_U_sol)
 cat("r :", CI_r, "\n")
 
-#CI_lam <- 1/thetas2
 CI_lam <- exp(CI_r)
 cat("lambda:", CI_lam, "\n")
 
-cat("\014")   # Clear screen
+fun <- function(x, t, g, c) {
+  th <- exp(-x*t)
+  sum(th * g) - c
+}
 
-# Results:
-cat("Initial number of individuals N :", N)
-cat("Offspring size K :", K)
-cat("R0 :", R0)
-cat("Longevity :",CI_L)
-cat("Generation time :",CI_mu)
-cat("r :", CI_r)
-cat("lambda:", CI_lam)
+clear_screen()
 
-cat("New data saved to:  ", fnam2, "\n")
+# RESULTS
+cat("Initial number of individuals N :", N, "\n")
+cat("Offspring size K :", K, "\n")
+cat("R0 :", R0, "\n")
+cat("Longevity :", CI_L, "\n")
+cat("Generation time :", CI_mu, "\n")
+cat("r :", CI_r, "\n")
+cat("lambda:", CI_lam, "\n")
+
+cat("New data saved to: ", fnam2, "\n")
 
 
+           
